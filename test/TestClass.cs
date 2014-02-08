dd048083c39ab33df380f901aaa7479ce4e6b6d2
liqiangtq <liqiang@tq-soft.com>
2014-02-08 11:24:31 +0800

    Add a new class.
    This is a test class.

---
+++ test/TestClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    //LiQiang add on 02/08/2014
+    class TestClass
+    {
+        protected int TestFun()
+        {
+            return 100;
+        }
+    }
+}
