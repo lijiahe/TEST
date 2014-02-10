12a36ef06fb10fa0af93538d7fcb20fac98358a2
ShiWeitq <shiwei@tq-soft.com>
2014-02-10 15:43:31 +0800

    Revert "add  some  methods"
    This reverts commit c97e6bc4dbfba417389c00d65462e78caaba3d5a.

--- test/ClassForShiwei.cs
+++ test/ClassForShiwei.cs
@@ -8,17 +8,5 @@ namespace test
 {
     class ClassForShiwei
     {
-        private string Test1()
-        {
-            return "This is a test1";
-        }
-        private string Test2()
-        {
-            return "This is a test2";
-        }
-        private string Test3()
-        {
-            return "This is a test3";
-        }
     }
 }
