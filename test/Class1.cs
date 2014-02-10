21a7a0bc2c43558583cf2974e02820f255d2c03d
ShiWeitq <shiwei@tq-soft.com>
2014-02-10 12:33:27 +0800

    Revert "Delete Class1.cs"
    This reverts commit 1b200f63fb482cb045710bf7a5651ff017946518.

---
+++ test/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    public class Class1
+    {
+        private string FirstMethod()
+        {
+            // tom 2014-02-04
+            //return -1;
+
+            // second change 
+            return " work on master branch";
+        }
+
+
+        private string SecondMethod()
+        {
+            // tom 2014-02-04
+            return "add 2nd method in develop branch";
+        }
+ 
+        private string NeedLiqiangModify()
+        {
+            //waiting for liqiang implement
+            return "";
+        }
+
+        private string NeedShiweiModify()
+        {
+            // waiting for shiwei implement
+            string strTest = "this is a test";
+            return strTest;
+        }
+
+        private string NeedLiuHaoModify()
+        {
+            // waiting for LiuHao implement
+            return "";
+        }
+    }
+}
