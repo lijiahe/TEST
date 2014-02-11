a497d8fca41a318d694051e78d5fa2699c59354f
ShiWeitq <shiwei@tq-soft.com>
2014-02-11 17:12:51 +0800

    Modify ClassForShiWei
    Modify ClassForShiWei

---
+++ test/ClassForShiwei.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class ClassForShiwei
+    {
+       class ClassForShiwei
+       {  
+           //shiwei add 02/11/2014      
+           private string TestMethod()  
+           {           
+               return "This is a test.";   
+           }   
+       }
+    }
+}
